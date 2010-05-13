67cf1f6627148761fb7ba997b68a19ecc6fdf59c
SStaudenmeyer <phpmywave@googlemail.com>
2010-05-13 16:45:10 +0200

    Added Documentation for Index-Controller
    Added Documentation Sections for the class, init and indexAction Methods.

--- phpmywave/application/controllers/IndexController.php
+++ phpmywave/application/controllers/IndexController.php
@@ -1,10 +1,37 @@
 <?php
+
+/**
+ * Index Controller - gets the application to run
+ *
+ * The Index-Controller recieves the starting command directly from the bootstraping Index-File. It
+ * keeps the process running and delegates Tasks.
+ *
+ * @category   Zend
+ * @package    Zend_Controller
+ * @subpackage Application
+ * @link       http://www.phpmywave.org
+ */
 class IndexController extends Zend_Controller_Action
 {
+
+    /**
+     * Function: init()
+     *
+     * Basic initialization method, no magic available here yet.
+     *
+     * @return NULL;
+     */
     public function init()
     {
     }
 
+    /**
+     * Function: indexAction()
+     *
+     * This Action is run from the Index-File. Basic hello-world-page.
+     *
+     * @returns string  $hellpWorld     Simply echoes: "Hello phpMyWave!!!"
+     */
     public function indexAction()
     {
         new phpMyWave_Example();
