9c8d3c6a1925607af76a3064b6b5437231f249c2
mschroeder <mschroeder@github.x-home.org>
2010-05-13 21:07:08 +0200

    Added abstract Event class

---
+++ phpmywave/library/phpMyWave/Event.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * Abstract Event class
+ */
+abstract class phpMyWave_Event
+{
+
+    /**
+     * @var phpMyWave_Blip
+     */
+    private $_blip;
+
+    /**
+     * @var phpMyWave_EventMessageBundle
+     */
+    private $_bundle;
+
+    /**
+     * @var string
+     */
+    private $_modifierId;
+
+    /**
+     * @var long
+     */
+    private $_timestamp;
+
+    /**
+     * @var phpMyWave_EventType
+     */
+    private $_eventType;
+
+    /**
+     * @var phpMyWave_Wavelet
+     */
+    protected $_wavelet;
+
+    /**
+     *
+     * @param phpMyWave_EventType $_eventType
+     * @param phpMyWave_Wavelet $_wavelet
+     * @param phpMyWave_EventMessageBundle $_bundle
+     * @param string $_modifiedBy
+     * @param long $_timestamp
+     * @param string $_blipId
+     */
+    protected function __construct($_eventType, $_wavelet, $_bundle,
+                                   $_modifiedBy, $_timestamp, $_blipId)
+    {
+        ;
+    }
+
+    /**
+     * Returns the blip in which this event occurs, or the root blip for a
+     * wavelet event
+     *
+     * @return phpMyWave_Blip
+     */
+    public function getBlip()
+    {
+        return $this->_blip;
+    }
+
+    /**
+     * Returns the message bundle which this event belongs to
+     *
+     * @return phpMyWave_EventMessageBundle
+     */
+    public function getBundle()
+    {
+        return $this->_bundle;
+    }
+
+    /**
+     * Returns the id of the participant that triggered this event
+     *
+     * @return string
+     */
+    public function getModifiedBy()
+    {
+        return $this->_modifierId;
+    }
+
+    /**
+     * Returns the timestamp when this event occurred on the server
+     *
+     * @return long
+     */
+    public function getTimestamp()
+    {
+        $this->_timestamp;
+    }
+
+    /**
+     * Returns the type of the event
+     *
+     * @return phpMyWave_EventType
+     */
+    public function getType()
+    {
+        $this->_eventType;
+    }
+
+    /**
+     * Returns the wavelet in which this event occurs
+     *
+     * @return phpMyWave_Wavelet
+     */
+    public function getWavelet()
+    {
+        return $this->_wavelet;
+    }
+
+}
