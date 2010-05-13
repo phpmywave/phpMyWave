9a2dd8789d9644eecbc5a734920cbd06b7f8a33c
dprokscha <daniel.prokscha@mayflower.de>
2010-05-13 11:12:09 +0200

    added proxy

--- gwave2twitter/main.py
+++ gwave2twitter/main.py
@@ -1,23 +1,67 @@
-from waveapi import events
-from waveapi import robot
-from waveapi import appengine_robot_runner
+#!/usr/bin/python2.5
+#
+# Copyright 2009 Google Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import logging
 
-def OnWaveletSelfAdded(event, wavelet):
-  """Invoked when the robot has been added."""
-  logging.info("OnWaveletSelfAdded called")
-  wavelet.reply("\nHi everybody! I'm a Python robot!")
-  
-def OnWaveletParticipantsChanged(event, wavelet):
-  logging.info("OnParticipantsChanged called")
-  newParticipants = event.participants_added
-  for newParticipant in newParticipants:
-    wavelet.reply("\nHi : " + newParticipant)
+from google.appengine.api import urlfetch
+from google.appengine.ext import webapp
+import google.appengine.ext.webapp.util
 
-if __name__ == '__main__':
-  myRobot = robot.Robot('v2waveapi', 
-      image_url='http://v2waveapi.appspot.com/icon.png',
-      profile_url='http://v2waveapi.appspot.com/')
-  myRobot.register_handler(events.WaveletParticipantsChanged, OnWaveletParticipantsChanged)
-  myRobot.register_handler(events.WaveletSelfAdded, OnWaveletSelfAdded)
-  appengine_robot_runner.run(myRobot)
+REMOTE_ROBOT = 'http://demo.phpmywave.org/?'
+ATTEMPTS = 3
+
+
+class Proxy(webapp.RequestHandler):
+
+  def _forward(self, method):
+    path = REMOTE_ROBOT + self.request.path
+    logging.info('path: <%s>', path)
+    robot_response = None
+    for i in range(ATTEMPTS):
+      try:
+        robot_response = urlfetch.fetch(path, method=method, deadline=10,
+                                        payload=self.request.body)
+        logging.info('remote response: %d', robot_response.status_code)
+        break
+      except urlfetch.DownloadError, e:
+        logging.error('Download error: %s', e)
+    if not robot_response:
+      raise RuntimeError(
+        'Did not receive response from %s after %s tries. '
+        'Either the host is down or urlfetch is still broken.'
+        % (path, ATTEMPTS))
+    self.response.set_status(robot_response.status_code)
+    # Only copy over Content-Type header for now
+    for header in ['Content-Type']:
+      if header in robot_response.headers:
+        self.response.headers[header] = robot_response.headers[header]
+    self.response.out.write(robot_response.content)
+
+  def get(self):
+    self._forward('GET')
+
+  def post(self):
+    self._forward('POST')
+
+
+def main():
+  app = webapp.WSGIApplication([('.*', Proxy)], debug=True)
+  webapp.util.run_wsgi_app(app)
+
+
+if __name__=='__main__':
+  main()
+  
